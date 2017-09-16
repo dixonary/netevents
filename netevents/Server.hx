511bf375e4ebbbeb9f73a7d6c772196e7d2f5448
dixonary <alex@dixonary.co.uk>
2017-09-16 17:32:46 +0100

    blocking

--- netevents/Server.hx
+++ netevents/Server.hx
@@ -137,7 +137,7 @@ class Server {
                 mutex.release();
             }
             catch(e:Dynamic) {
-                print(e.indexOf("Blocked"));
+                print("itwas", e.indexOf("Blocked"));
                 if(e.indexOf("Blocked") != -1) continue;
                 print("err", '$e');
                 print("err", haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
